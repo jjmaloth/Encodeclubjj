571e6543dfa700d48f19b4a1d214ef04d0a6ab19
Tsar-Odragde <odragde@live.com>
2025-03-15 21:14:18 -0400

    Implementation of Ballot.sol

--- Week-2/hardhat.config.ts
+++ Week-2/hardhat.config.ts
@@ -1,8 +1,21 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+// console.log("providerApiKey", providerApiKey);
+// console.log("deployerPrivateKey", deployerPrivateKey);
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+  },
 };
 
 export default config;
